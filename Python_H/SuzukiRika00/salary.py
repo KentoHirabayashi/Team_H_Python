aff9a888641dc86d2f4f5023cd92d7ffeec6ba47
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-05-18 08:11:11 +0000

    salary.pyを読みやすく修正

--- Python_H/SuzukiRika00/salary.py
+++ Python_H/SuzukiRika00/salary.py
@@ -8,15 +8,14 @@ salary = int(args[1])
 if salary <= 1000000:
     tax_rate = 0.1
     tax = salary * tax_rate
-    tax = Decimal(str(tax)).quantize(Decimal("0"),
-    rounding=ROUND_HALF_UP)
-    pay_amount = salary - tax
-    print("支給額:{0}、税額:{1}".format(pay_amount, tax), end="")
+    
     #print("I don't like {}".format(food), end="")
 else:
     tax_rate = 0.2
     tax = (1000000 * 0.1) + (salary - 1000000) * tax_rate
-    tax = Decimal(str(tax)).quantize(Decimal("0"),
-    rounding=ROUND_HALF_UP)
-    pay_amount = salary - tax
-    print("支給額:{0}、税額:{1}".format(pay_amount, tax), end="")
+
+tax = Decimal(str(tax)).quantize(Decimal("0"),
+rounding=ROUND_HALF_UP)
+pay_amount = salary - tax
+print("支給額:{0}、税額:{1}".format(pay_amount, tax), end="")
+#print("支給額:"+ str(pay) + "、税額:" + str(tax), end="")
