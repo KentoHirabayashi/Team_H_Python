367666009b5b9abb31c7d7c40cf78c4d808c30ee
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-05-18 06:53:06 +0000

    変数名を”num”に修正

--- Python_H/SuzukiRika00/oddeven.py
+++ Python_H/SuzukiRika00/oddeven.py
@@ -2,9 +2,9 @@ import sys
 
 args = sys.argv
 
-input_number1 = args[1]
+num = args[1]
 
-if int(input_number1) %2 == 1:
+if int(num) %2 == 1:
     print("奇数", end="")
 else:
     print("偶数", end="")
