1aad4e8047af1371e630732caecc0303441d0674
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-05-22 08:09:20 +0000

    add　files

--- Python_H/SuzukiRika00/introfamily.py
+++ Python_H/SuzukiRika00/introfamily.py
@@ -1,7 +1,7 @@
 from introduce import Intro
 
 class IntroFam(Intro):
-    def __init__(self, name, age, cat): # コンストラクタ
+    def __init__(self, name, age, cat): # コンストラクタ（初期化メソッド）。たい焼きの型を温める
         self.name = name
         self.age = age
         self.cat = cat
@@ -11,3 +11,5 @@ class IntroFam(Intro):
         cattxt = "飼い猫の名前は、{0}です".format(self.cat)
         return cattxt 
     
+# __init__()メソッド
+# インスタンス生成時にこのメソッドが自動的に実行され、勝手に初期化してくれる
