a338d64754a2ed1730bff95f1a4995885a86d6c7
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-05-24 02:36:13 +0000

    add aqua

---
+++ Python_H/SuzukiRika00/aquacalc.py
@@ -0,0 +1,23 @@
+import datetime
+import sys
+args = sys.argv
+
+input_date = str(args[1]) #日付
+adult_num = int(args[2]) #人数
+child_num = int(args[3]) #人数
+
+dt = datetime.datetime.strptime(input_date, "%Y%m%d")
+youbi = datetime.datetime.strftime(dt,"%a")
+print(youbi)
+#土日
+if youbi == 'Sat' or youbi == 'San':
+    #大人2400、子供1500
+    adult_fee = 2400 * adult_num
+    child_fee = 1500 * child_num
+else:
+    #大人2000、子供1200
+    adult_fee = 2000 * adult_num
+    child_fee = 1200 * child_num
+
+sum = adult_fee + child_fee
+print(sum)
